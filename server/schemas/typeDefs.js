e92592b6ff3959724e42a55291e8fdc3ec0062d5
Trevor Pino <trevorpino@yahoo.com>
2020-12-27 09:14:09 -0700

    added user and book

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,23 @@
 const { gql } = require('apollo-server-express');
-const { typeDefs } = require('.');
+// const { typeDefs } = require('.');
 
 typeDefs = gql`
 
+    type user {
+        _id: String
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [book]
+    }
+    type book {
+        bookId: String
+        authors:[String]
+        description: String
+        title: String
+        img: String
+        link: String
+    }
 
 `;
 
